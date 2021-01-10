7aace9b2001ba938e1910d4dcb7fa3c86eb9863e
nitinkandikatla <nitinkandikatla99@gmail.com>
2021-01-10 22:58:41 +0530

    ADDED : calculate wage for monthg function added

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,9 +3,10 @@ public class EmployeeWage {
 		public static final int IS_PART_TIME=1;
 		public static final int IS_FULL_TIME=2;
 		public static final int EMP_RATE_PER_HR=20;
+		public static final int NUM_WORKING_DAYS=20;
 		public static void main(String[] args) {
-			int empHrs=0;
-			int empWage=0;
+			int empHrs=0,empWage=0,totalWage=0;
+			for(int day=0;day<NUM_WORKING_DAYS;day++) {
 			int empCheck=(int)Math.floor(Math.random()*10)%3;
 			switch(empCheck) {
 			case IS_PART_TIME: 
@@ -18,8 +19,12 @@ public class EmployeeWage {
 				empHrs=0;
 			}
 		empWage=empHrs*EMP_RATE_PER_HR;
+		totalWage+=empWage;
 		System.out.println("Emp Wage : " + empWage);
+
 	}
+		System.out.println("Emp total Wage:" + totalWage);
+}
 }
 
 
