b6cba596e7778e1f7d2c697a86af123021e85fb6
nitinkandikatla <nitinkandikatla99@gmail.com>
2021-01-10 22:41:46 +0530

    ADDED : added employee present or absent functionality

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,6 +1,11 @@
 public class EmployeeWage {
 	public static void main(String[] args) {
-		System.out.println("Welcome");
+		int IS_FULL_TIME=1;
+		double empCheck=Math.floor(Math.random()*10)%2;
+		if(empCheck==IS_FULL_TIME) 
+			System.out.println("Employee is present");
+		else
+			System.out.println("Employee is absent");
 	}
 
 }
