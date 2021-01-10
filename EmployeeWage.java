8f39221301e38b152ccf5089092a78d855c71ff7
nitinkandikatla <nitinkandikatla99@gmail.com>
2021-01-10 22:50:42 +0530

    ADDED : added part time

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,19 +1,17 @@
 public class EmployeeWage {
 	public static void main(String[] args) {
-		int IS_FULL_TIME=1;
-		int EMP_RATE_PER_HR=20;
-		int empHrs=0;
-		int empWage=0;
-		double empCheck=Math.floor(Math.random()*10)%2;
-		if(empCheck==IS_FULL_TIME) {
-			empHrs=8;
-			System.out.println("Employee is present");
-		}
-		else
-			System.out.println("Employee is absent");
-		empWage=empHrs*EMP_RATE_PER_HR;
-		System.out.println("Emp Wage : " + empWage);
-		}
-
+	int IS_PART_TIME=1;
+	int IS_FULL_TIME=2;
+	int EMP_RATE_PER_HR=20;
+	int empHrs=0;
+	int empWage=0;
+	double empCheck=Math.floor(Math.random()*10)%2;
+	if(empCheck==IS_FULL_TIME) 
+		empHrs=8;
+	else if(empCheck==IS_PART_TIME)
+		empHrs=4;
+	empWage=empHrs*EMP_RATE_PER_HR;
+	System.out.println("Emp Wage : " + empWage);
+}
 }
 
