7f51ab713488bcb54a70b60f27a83dc47bbdc428
nitinkandikatla <nitinkandikatla99@gmail.com>
2021-01-12 12:23:58 +0530

    ADDED : function for multiple company

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,9 +2,6 @@ public class EmployeeWage {
 	
 		public static final int IS_PART_TIME=1;
 		public static final int IS_FULL_TIME=2;
-		public static final int EMP_RATE_PER_HR=20;
-		public static final int NUM_WORKING_DAYS=20;
-		public static final int MAX_WORKING_HRS=100;
 		
 		public static int computeEmpWage(String company,int EMP_RATE_PER_HR,int NUM_WORKING_DAYS,int MAX_WORKING_HRS) {
 			int empHrs=0,totalEmpHrs=0,totalWorkingDays=0;
