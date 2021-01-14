99f3aac9017ea4e844ed5353c8385f3a668ca1d8
nitinkandikatla <nitinkandikatla99@gmail.com>
2021-01-14 21:40:41 +0530

    ADDED : manage employee for multiple company

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,22 +1,64 @@
+package employeeWage;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 class CompanyEmpWage {
 
 	
 		public static final int IS_PART_TIME=1;
 		public static final int IS_FULL_TIME=2;
 		
-		private final String company;
-		private final int EMP_RATE_PER_HR;
-		private final int NUM_WORKING_DAYS;
-		private final int MAX_WORKING_HRS;
-		private int totalEmpWage;
+		public String company;
+		public int EMP_RATE_PER_HR;
+		public int NUM_WORKING_DAYS;
+		public int MAX_WORKING_HRS;
+		public int totalEmpWage;
 		
-		public CompanyEmpWage(String company,int EMP_RATE_PER_HR,int NUM_WORKING_DAYS,int MAX_WORKING_HRS) {
+		public CompanyEmpWage() {
 			
-		    this.company=company;
-			this.EMP_RATE_PER_HR=EMP_RATE_PER_HR;
-			this.NUM_WORKING_DAYS=NUM_WORKING_DAYS;
-			this.MAX_WORKING_HRS=MAX_WORKING_HRS;
 		}
+		
+		public String getCompany() {
+			return company;
+		}
+
+		public void setCompany(String company) {
+			this.company = company;
+		}
+
+		public int getEMP_RATE_PER_HR() {
+			return EMP_RATE_PER_HR;
+		}
+
+		public void setEMP_RATE_PER_HR(int eMP_RATE_PER_HR) {
+			EMP_RATE_PER_HR = eMP_RATE_PER_HR;
+		}
+
+		public int getNUM_WORKING_DAYS() {
+			return NUM_WORKING_DAYS;
+		}
+
+		public void setNUM_WORKING_DAYS(int nUM_WORKING_DAYS) {
+			NUM_WORKING_DAYS = nUM_WORKING_DAYS;
+		}
+
+		public int getMAX_WORKING_HRS() {
+			return MAX_WORKING_HRS;
+		}
+
+		public void setMAX_WORKING_HRS(int mAX_WORKING_HRS) {
+			MAX_WORKING_HRS = mAX_WORKING_HRS;
+		}
+
+		public int getTotalEmpWage() {
+			return totalEmpWage;
+		}
+
+		public void setTotalEmpWage(int totalEmpWage) {
+			this.totalEmpWage = totalEmpWage;
+		}
+
 		public void computeEmpWage() {
 			
 			int empHrs=0,totalEmpHrs=0,totalWorkingDays=0;
@@ -34,29 +76,49 @@ class CompanyEmpWage {
 						empHrs=0;
 				}
 				totalEmpHrs+=empHrs;
-				System.out.println("Day:" +totalWorkingDays + " Emp hrs:" + empHrs);
+				System.out.println("Day:" +totalWorkingDays + " Emp hrs:" + empHrs + " Daily wage: " + empHrs*EMP_RATE_PER_HR + " total wage :" + totalEmpHrs*EMP_RATE_PER_HR);
+				
 			}
+			System.out.println(totalEmpHrs);
 			totalEmpWage=totalEmpHrs*EMP_RATE_PER_HR;
-			
+			setTotalEmpWage(totalEmpWage);
+			//return totalEmpWage;
 		}
-		
+
 		@Override
 		public String toString() {
-			return "Total Emp Wage for Company "+company+" is "+totalEmpWage;
+			return "Company Details [company=" + company + ", EMP_RATE_PER_HR=" + EMP_RATE_PER_HR + ", NUM_WORKING_DAYS="
+					+ NUM_WORKING_DAYS + ", MAX_WORKING_HRS=" + MAX_WORKING_HRS + ", totalEmpWage=" + totalEmpWage
+					+ "]";
 		}
+		
+		
+
 }
 public class EmployeeWage {
 		public static void main(String[] args) {
-			    
-				CompanyEmpWage c[]=new CompanyEmpWage[3];
-				c[0]=new CompanyEmpWage("Dmart",20,20,100);
-				c[1]=new CompanyEmpWage("Reliance",15,18,50);
-				c[2]=new CompanyEmpWage("Tesla",25,25,120);
-				for(int i=0;i<c.length;i++)
-	    		{
-	        		c[i].computeEmpWage();
-	        		System.out.println(c[i]);
-			    }
-		}
+			
+			ArrayList<CompanyEmpWage> company=new ArrayList<>();
+			System.out.println("Enter how companys to add");
+			Scanner sc =new Scanner(System.in);
+			int companyCount=sc.nextInt();
+			CompanyEmpWage c[]=new CompanyEmpWage[companyCount];
+			for(int i=0;i<companyCount;i++) {
+				
+				Scanner s =new Scanner(System.in);
+				c[i]=new CompanyEmpWage();
+				System.out.println("Enter company name:");
+				c[i].company=s.nextLine();
+				System.out.println("Enter rate per hr");
+				c[i].EMP_RATE_PER_HR=s.nextInt();
+				System.out.println("number of working days:");
+				c[i].NUM_WORKING_DAYS=s.nextInt();
+				System.out.println("Enter max hrs");
+				c[i].MAX_WORKING_HRS=s.nextInt();
+				company.add(c[i]);
+				c[i].computeEmpWage();
+			}
+			System.out.println(company);
 	
+}
 }
